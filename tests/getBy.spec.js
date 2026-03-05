ac3a152163a09ecdc7cb34725f54b002a4da0e00
helen_yrofeeva <helen_yrofeeva@epam.com>
2026-03-05 14:09:39 +0300

    one file

--- tests/getBy.spec.js
+++ tests/getBy.spec.js
@@ -31,4 +31,33 @@ test.describe('Locator API - getBy - Tests', () => {
     await expect(page.getByRole('img', { name: 'Healenium Logo' })).toBeVisible({ TIMEOUT });
   });
 
+  test('click action', async ({ page }) => {
+    test.slow();
+
+    const inputField = page.locator('.test_class');
+    await inputField.click({ timeout: TIMEOUT });
+
+    // Click Change locators button to test healing
+    const submitBtn = page.locator('#Submit');
+    await submitBtn.click();
+
+    // Test healing - same action should work after locator change
+    const healedInputField = page.locator('.test_class');
+    await healedInputField.click({ timeout: TIMEOUT });
+  });
+
+  test('double click action', async ({ page }) => {
+    test.slow();
+    const inputField = page.locator('input#change_id');
+    await inputField.dblclick({ timeout: TIMEOUT });
+
+    // Click Change locators button to test healing
+    const submitBtn = page.locator('#Submit');
+    await submitBtn.click();
+
+    // Test healing - same action should work after locator change
+    const healedInputField = page.locator('input#change_id');
+    await healedInputField.dblclick({ timeout: TIMEOUT });
+  });
+  
 });
